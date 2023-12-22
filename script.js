392586f295300294bc23ead08d629476e05e90b3
fallermj <150002517+fallermj@users.noreply.github.com>
2023-12-23 02:02:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ menu.onclick = () => {
   navbar.classList.toggle('active');
   
 }
+
+//dark Mode 
+let darkmode = document.querySelector('#darkmode');
+darkmode.onclick = () => {
+	if(darkmode.classList.contains('bx-moon')){
+		darkmode.classList.replace('bx-moon', 'bx-sun');
+		document.body.classList.add('active');
+	}
+	else {
+		darkmode.classList.replace('bx-sun','bx-moon');
+		document.body.classList.remove('active');
+	}
+}
